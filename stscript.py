bc959d6d3723af1a5a25226ebc28e29c06c3c505
craventhehunter <cravenserge@gmail.com>
2017-01-04 16:43:35 -0800

    Update stscript.py

--- stscript.py
+++ stscript.py
@@ -4,19 +4,15 @@ import string
 def process_json(filename):
     with open(filename, 'r') as fp:
         raw_string = fp.read()
-        replaced_string = raw_string.replace('}\n{', '},\n{')
-        s = "[" + replaced_string + "]"
-        streamdata = json.loads(s)
-        s1 = streamdata['list']
-        #s = str(streamdata)
-        all_data = open("json_output.txt", "w")
-        all_data.write(s1)
-        all_data.close()		
-        #for info in streamdata:
-            #if ['v'] in streamdata > 1:
-        print s1[0]
-             #print error
+        mtu_dicts = {}
+        for line in raw_string.splitlines():
+         if ':"mtu"' in line:
+          mtu_values = json.loads(line).get("v")
+          print mtu_values
+         if '"server_mtu"' in line:
+          smtu_values = json.loads(line).get("v")
+          print smtu_values
 
-		
+
 if __name__ == "__main__":
     process_json('practice_data.json')
