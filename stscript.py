83accc504429ac8187aea23eb4467993459cda23
craventhehunter <cravenserge@gmail.com>
2017-01-04 16:44:08 -0800

    Update stscript.py

--- stscript.py
+++ stscript.py
@@ -1,5 +1,6 @@
 import json
 import string
+import re
 
 def process_json(filename):
     with open(filename, 'r') as fp:
@@ -8,10 +9,9 @@ def process_json(filename):
         for line in raw_string.splitlines():
          if ':"mtu"' in line:
           mtu_values = json.loads(line).get("v")
-          print mtu_values
          if '"server_mtu"' in line:
           smtu_values = json.loads(line).get("v")
-          print smtu_values
+          
 
 
 if __name__ == "__main__":
